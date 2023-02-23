3d60609e19f532acd09e1627698edfb8c86842b3
Richard McHorgh <mchorghr2@gmail.com>
2023-02-23 13:32:48 -0500

    added globe and starry background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
 	import './globals.sass';
+	import Globe from './Globe.svelte';
 </script>
 
-<h1>WORLD</h1>
-<main />
+<section>
+	<h1>CSA</h1>
+	<h2>TO THE</h2>
+	<h1>WORLD</h1>
+	<Globe />
+</section>
 
 <style lang="sass">
+    h1, h2
+        color: white
+
+    :global(.globe)
+        position: absolute
+
+    section
+        height: 100%
+        display: flex
+        flex-direction: column
+        align-items: center
+        justify-content: center
 
 </style>
---
+++ src/routes/Globe.svelte
@@ -0,0 +1,12 @@
+<div class="globe" />
+
+<style lang="sass">
+    $size: 80vw
+
+    .globe
+        border-radius: 100%
+        background-color: #3a6eb4
+        width: $size
+        height: $size
+        bottom: $size * -0.5
+</style>
